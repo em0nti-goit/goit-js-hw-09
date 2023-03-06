b7dc3e099d8c869248ba116b2ca8f79721f5b0f1
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-03-06 22:05:59 +0200

    complete task 01: change the <body> background color

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+
+const startButtonRef = document.querySelector('button[data-start]');
+const stopButtonRef = document.querySelector('button[data-stop]');
+let intervalId = 0;
+
+startButtonRef.addEventListener('click', onClickStartButton);
+stopButtonRef.addEventListener('click', onClickStopButton);
+
+function onClickStartButton() {
+  this.setAttribute('disabled', 'disabled');
+  intervalId = setInterval(() => document.body.style.backgroundColor = getRandomHexColor(), 1000);
+}
+
+function onClickStopButton() {
+  clearInterval(intervalId);
+  startButtonRef.removeAttribute('disabled');
+}
+
 
