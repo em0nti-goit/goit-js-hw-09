41cde8831892befda886647611227e83a318556c
Andrii Triukhan <39101979+em0nti@users.noreply.github.com>
2023-03-11 14:44:55 +0200

    complete the task #2: countdown timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,58 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { convertMs, addLeadingZero } from "./convert-ms.mjs";
+
+const TIMER_STEP = 1000;
+//restart timer after click on input
+let timerStartDate = 0;
+
+const startButtonRef = document.querySelector("button[data-start]");
+const timerRefs = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+}
+
+const flatpickrOptions = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const start = Date.now();
+    const end = selectedDates[0].getTime();
+    if (end < start) {
+      console.log("Please choose a date in the future");
+    } else {
+      startButtonRef.removeAttribute("disabled");
+      timerStartDate = end - start;
+    }
+  },
+};
+
+// create flatpickr instance with options and attach it to the input field
+
+flatpickr("input#datetime-picker", flatpickrOptions);
+
+startButtonRef.addEventListener("click", () => {
+  console.log("Timer started");
+
+  let currentTimerValue = timerStartDate;
+
+  const timerId = setInterval(() => {
+    currentTimerValue -= TIMER_STEP;
+
+    console.log(convertMs(currentTimerValue));
+
+    timerRefs.days.textContent = (convertMs(currentTimerValue).days < 10) ? addLeadingZero(convertMs(currentTimerValue).days) : convertMs(currentTimerValue).days;
+    timerRefs.hours.textContent = addLeadingZero(convertMs(currentTimerValue).hours);
+    timerRefs.minutes.textContent = addLeadingZero(convertMs(currentTimerValue).minutes);
+    timerRefs.seconds.textContent = addLeadingZero(convertMs(currentTimerValue).seconds);
+
+    if (currentTimerValue <= 0) {
+      clearInterval(timerId);
+      console.log("Timer finished");
+    }
+  }, TIMER_STEP);
+});
